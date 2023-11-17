bd20e9b8f4a6d048ccbddec85cc9108b29a357a8
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-11-17 15:21:04 +0700

    sulit

--- app/src/main/java/com/example/a125_ucp2/PengelolaHalaman.kt
+++ app/src/main/java/com/example/a125_ucp2/PengelolaHalaman.kt
@@ -1,2 +1,18 @@
 package com.example.a125_ucp2
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+enum class PengelolaHalaman {
+    Home,
+    Formulir,
+    Rasa
+}
+@Composable
+fun EsJumboAppbar(
+    bisaNavigasiBack: Boolean,
+    navigasiUp: () -> Unit,
+    modifier: Modifier = Modifier
+){
+}
+
